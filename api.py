883792ab723e9991291f2e037a246d563ac07459
Ramon Amini <ograaamonnn@gmail.com>
2020-09-14 20:51:29 -0700

    Gets words from actual DB now

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -15,6 +16,13 @@ words = [
      'EnglishWord': 'Naan',
      'DariWord': "Bread"}
 ]
+#
+def dict_factory(cursor, row):
+    d = {}
+    for index, col in enumerate(cursor.description):
+        d[col[0]] = row[index]
+    print(d)
+    return d
 
 
 @app.route('/', methods=['GET'])
@@ -25,37 +33,73 @@ def home():
 # A route to return all of the available entries in our catalog.a
 @app.route('/words/all', methods=['GET'])
 def api_all():
-    return jsonify(words)
 
+    conn = sqlite3.connect('words.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_words = cur.execute('SELECT * FROM words;').fetchall()
+
+    return jsonify(all_words)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 @app.route('/words', methods=['GET'])
-def api_id():
-    results = []
+def api_filter():
+    query_parameters = request.args
+    id = query_parameters.get('id')
+    word = query_parameters.get('word')
 
-    if 'id' in request.args:
-        requestid = request.args.get('id')
-        for word in words:
-            if word['id'] == requestid:
-                results.append(word)
+    query = "SELECT * FROM words WHERE"
+    to_filter = []
 
-
-    #not working for some reason
-    elif 'word' in request.args:
-        requestWord = request.args.get('word')
-        requestWord = str(requestWord)
-        requestWord = requestWord.capitalize()
-        print(requestWord)
-        for word in words:
-            print(word['DariWord'] == requestWord)
-            if word['EnglishWord'] == requestWord or word['DariWord'] == requestWord:
-                results.append(word)
-
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    elif word:
+        query += ' english=? AND'
+        to_filter.append(word)
     else:
-        return "Error: No valid field provided. Please specify an id or word."
+        return page_not_found(404)
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('words.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
     return jsonify(results)
+
+# @app.route('/words', methods=['GET'])
+# def api_id():
+#     results = []
+#
+#     if 'id' in request.args:
+#         requestid = request.args.get('id')
+#         for word in words:
+#             if word['id'] == requestid:
+#                 results.append(word)
+#
+#     elif 'word' in request.args:
+#         requestWord = request.args.get('word')
+#         requestWord = str(requestWord)
+#         requestWord = requestWord.capitalize()
+#         print(requestWord)
+#         for word in words:
+#             print(word['DariWord'] == requestWord)
+#             if word['EnglishWord'] == requestWord or word['DariWord'] == requestWord:
+#                 results.append(word)
+#
+#     else:
+#         return "Error: No valid field provided. Please specify an id or word."
+#
+#     # Use the jsonify function from Flask to convert our list of
+#     # Python dictionaries to the JSON format.
+#     return jsonify(results)
 
 if __name__ == '__main__':
     app.run()
